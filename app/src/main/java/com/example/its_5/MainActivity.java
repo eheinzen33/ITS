cc973d5ef90af157d4f0096a66418f4ca2774181
eheinzen33 <63426413+eheinzen33@users.noreply.github.com>
2022-11-09 20:23:56 -0300

    Advances - TEST JUANMI

--- app/src/main/java/com/example/its_5/MainActivity.java
+++ app/src/main/java/com/example/its_5/MainActivity.java
@@ -15,28 +15,28 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Button showProductsBtn = findViewById(R.id.buttonProducts);
-
-        showProductsBtn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Handler handler = new Handler(Looper.getMainLooper());
-                handler.post(new Runnable() {
-                    @Override
-                    public void run() {
-                        FetchData fetchData = new FetchData("https://its-database-connection.azurewebsites.net/select_products.php");
-                        if (fetchData.startFetch()) {
-                            if (fetchData.onComplete()) {
-                                String result = fetchData.getResult();
-                                System.out.println(result.getClass());
-                                System.out.println(result);
-                                //End ProgressBar (Set visibility to GONE)
-                            }
-                        }
-                    }
-                });
-            }
-        });
+//        Button showProductsBtn = findViewById(R.id.buttonProducts);
+//
+//        showProductsBtn.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                Handler handler = new Handler(Looper.getMainLooper());
+//                handler.post(new Runnable() {
+//                    @Override
+//                    public void run() {
+//                        FetchData fetchData = new FetchData("https://its-database-connection.azurewebsites.net/select_products.php");
+//                        if (fetchData.startFetch()) {
+//                            if (fetchData.onComplete()) {
+//                                String result = fetchData.getResult();
+//                                System.out.println(result.getClass());
+//                                System.out.println(result);
+//                                //End ProgressBar (Set visibility to GONE)
+//                            }
+//                        }
+//                    }
+//                });
+//            }
+//        });
 
 
 
